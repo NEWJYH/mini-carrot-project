1712e1fb503747f1ff0337a43ea5d0607b59a33a
NEWJYH <joung5846@naver.com>
2023-02-23 17:06:26 +0900

    도커 기본설정

--- carrot/src/app.module.ts
+++ carrot/src/app.module.ts
@@ -14,11 +14,11 @@ import { UserModule } from './apis/users/user.module';
 
     TypeOrmModule.forRoot({
       type: 'mysql', // 데이터 베이스 타입
-      host: 'localhost', // local 환경으로 진행
+      host: 'localhost', // local 환경으로 진행 // docker환경 my-database
       port: 3306, // mysql은 기본 port는 3306
       username: 'root', // mysql은 기본 user는 root로 지정
-      password: '12345678', // 본인의 mysql password
-      database: 'myproject03', // 연결할 데이터 베이스명
+      password: '12345678', // 본인의 mysql password // docker환경 root
+      database: 'carrot', // 연결할 데이터 베이스명
       entities: [__dirname + '/apis/**/*.entity.*'], // 데이터 베이스와 연결할 entity
       synchronize: true, // entity 테이블을 데이터베이스와 동기화할 것인지
       logging: true, // 콘솔 창에 log를 표시할 것인지
